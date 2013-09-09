0a41883a03da45cb493528c9a28193f51cb6dbea
fernando <fernanmatoro@gmail.com>
2013-09-09 01:41:10 -0600

    envio del username agregado

--- servidor-cliente.c
+++ servidor-cliente.c
@@ -12,9 +12,16 @@ int main()
 
  int sock;
  char datosrecibidos[2048];    //arreglo para recibir los datos  
- char datosenviados[2048]; 
+ char datosenviados[2048];
+ char nombreenviado[15];
+ char nombrerecibido[15]; 
  struct sockaddr_in servidor; //struct obligatorio   para puertos y almacenar direcciones
  int var1,misock;
+
+ printf("digite su username: ");
+ gets(nombreenviado);
+ strcat(nombreenviado,":");
+
  sock = socket(AF_INET, SOCK_STREAM, 0);  
  int id;     
  if(sock ==-1)
@@ -47,17 +54,17 @@ int main()
   {
 	  //printf("hola\n");
 	  if(fork()){
-		  
+		  recv(misock,nombrerecibido,sizeof(nombrerecibido),0);
 		  while(1){
 			  
-			  recv(misock,datosrecibidos,sizeof(datosrecibidos),0);                 
-			  printf("mensaje: %s\n",datosrecibidos);
+			  recv(misock,datosrecibidos,sizeof(datosrecibidos),0);
+                          printf("%s",nombrerecibido);                 
+			  printf("%s\n",datosrecibidos);
 
           		 }
 	  }
-	  else{
-		  while(1){
-			  //printf("digite su mensaje: ");
+	  else{   send(misock,nombreenviado,sizeof(nombreenviado),0);
+		  while(1){			 
 			  gets(datosenviados); 
 			  if(send(misock,datosenviados,sizeof(datosenviados),0)<0){
 				  perror("envio fallido\n");
