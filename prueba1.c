13f44c11eb3bd19387a62bfdb7760d7d66cbcbe6
alonjimo <alonjimo@gmail.com>
2013-09-09 21:59:39 -0600

    Cliente con waiting

--- prueba1.c
+++ prueba1.c
@@ -1,4 +1,4 @@
-5#include <stdio.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,65 +17,62 @@ int main()
  char datosrecibidos1[2048];
  const char * const mezcla = "\033[1;35m ";
  const char * const normal = "\033[1;32m";   
- struct hostent *hp;
+
 
  sock = socket(AF_INET,SOCK_STREAM,0);
  if( sock<0)
   {perror("no se creo el socket\n");
    exit(1);
   }
- else
-    {printf("se creo el socket\n");
-    }
-
+ 
  cliente.sin_family = AF_INET;
- cliente.sin_port = htons(9005);
- cliente.sin_addr.s_addr = INADDR_ANY;
- //inet_addr("172.16.1.129"); //Conexión compu-compu
+ cliente.sin_port = htons(4000);
+ cliente.sin_addr.s_addr = inet_addr("10.42.0.12");
  bzero(&(cliente.sin_zero),8);
-     
-
  
- //hp = gethostbyname ("10.42.0.1");
- //if(hp==0)
- //{
-  //perror("fallo conectar con el host\n");
-  //close(sock);
-  //exit(1);
- //}
-
+ int cont = 0;
  
- if(connect(sock,(struct sockaddr *)&cliente,sizeof(cliente))<0)
+ while(cont == 0 )
  {
-  perror("conexion fallida\n");
-  close(sock);
-  exit(1);
+	 if(connect(sock,(struct sockaddr *)&cliente,sizeof(cliente))>= 0){
+		 system("clear");
+		 printf("Contacto Disponible\n\n");
+		 cont++;
+	 }
+	 else{
+		 sleep(1);
+		 printf("Contacto no disponible, espera o intenta comunicarte mas tarde.\n");
+		 sleep(1);
+		 system("clear");
+	 }
+  
  }
- if(fork())
-          {
+ if(fork()){
 
-while(1){
-recv(sock,datosrecibidos,sizeof(datosrecibidos),0);
-printf("%s mensaje: %s %s \n",mezcla,datosrecibidos,normal);
+	while(1){
+		recv(sock,datosrecibidos,sizeof(datosrecibidos),0);
+		printf("%s mensaje: %s %s",mezcla,datosrecibidos,normal);
 
-}
+	}
 }
 
  else{
       while(1){
-//printf("digite su mensaje: ");
-gets(datosenviados);
-if(send(sock,datosenviados,sizeof(datosenviados),0)<0)
-               {
-perror("envio fallido\n");
-close(sock);
-exit(1);
-}
-printf("enviado con exito \n");
-}
+		printf("Alonso: ");
+		gets(datosenviados);
+		
+		if(send(sock,datosenviados,sizeof(datosenviados),0) < 0){
+			
+			perror("envio fallido\n");
+			close(sock);
+			exit(1);
+		}
 
-     }
+	}
+
+}
 
 return 0;
+
 }
 		 
